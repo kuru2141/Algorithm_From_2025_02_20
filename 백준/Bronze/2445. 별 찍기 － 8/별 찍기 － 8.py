42540e1ffaec2ad22c5a57b848f6e13a3f7d6e53
Jin <149752689+kuru2141@users.noreply.github.com>
2025-02-23 14:09:51 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.py
@@ -0,0 +1,26 @@
+N = int(input())
+
+for i in range(1, N + 1):
+    for j in range(i):
+        print('*', end = '')
+
+    for k in range((N - i)* 2):
+        print(' ', end = '')
+
+    for l in range(i):
+        print('*', end = '')
+
+    print('')
+
+
+for i in range(N - 1, 0, -1):
+    for j in range(i):
+        print('*', end = '')
+
+    for k in range((N - i)* 2):
+        print(' ', end = '')
+
+    for l in range(i):
+        print('*', end = '')
+
+    print('')
