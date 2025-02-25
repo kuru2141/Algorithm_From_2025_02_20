43bd347cef45c15d68d9bbc69c3acaaffff920aa
Jin <149752689+kuru2141@users.noreply.github.com>
2025-02-25 17:49:38 +0900

    [Gold V] Title: 토마토, Time: 2276 ms, Memory: 104684 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -10,18 +10,15 @@ def check_tomato(arr):
 M, N = map(int, input().split())
 
 arr = []
-visited = []
 queue = deque()
 
 for _ in range(N):
     arr.append(list(map(int, input().split())))
-    visited.append([False for _ in range(M)])
 
 for i in range(N):
     for j in range(M):
         if arr[i][j] == 1:
             queue.append((i, j, 0))
-            visited[i][j] = 1
 
 dr = [-1, 1, 0, 0]
 dc = [0, 0, -1, 1]
@@ -35,7 +32,7 @@ while queue:
         nr = r + dr[i]
         nc = c + dc[i]
 
-        if nr > -1 and nr < N and nc > -1 and nc < M and not arr[nr][nc] and not visited[nr][nc]:
+        if nr > -1 and nr < N and nc > -1 and nc < M and not arr[nr][nc]:
             queue.append((nr, nc, cnt + 1))
             list_cnt.append(cnt + 1)
             arr[nr][nc] = 1
