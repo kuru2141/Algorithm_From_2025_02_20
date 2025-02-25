e6feeeb9f26047dc27ef7060b155d26cb8c54a01
Jin <149752689+kuru2141@users.noreply.github.com>
2025-02-25 13:46:38 +0900

    [Gold IV] Title: 친구비, Time: 320 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Gold/16562. 친구비/친구비.py
@@ -0,0 +1,54 @@
+def make(N):
+    parents = []
+
+    for i in range(N + 1):
+        parents.append(i)
+
+    return parents
+
+def find(V):
+    if parents[V] == V:
+        return V
+
+    parents[V] = find(parents[V])
+    return parents[V]
+
+def union(a, b):
+    aRoot = find(a)
+    bRoot = find(b)
+    if aRoot == bRoot:
+        parents[a] = aRoot
+        parents[b] = bRoot
+        return False
+
+    parents[aRoot] = bRoot
+    return True
+
+N, M, k = map(int, input().split())
+money = list(map(int, input().split()))
+
+parents = make(N)
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    union(a, b)
+
+for i in range(N):
+    find(i)
+
+parents = parents[1:] # 0번째 친구 제거
+dic = dict()
+
+for i in range(len(parents)):
+    ele = parents[i]
+    if ele not in dic:
+        dic[ele] = money[i]
+    else:
+        if dic[ele] > money[i]:
+            dic[ele] = money[i]
+
+sum_dict = 0
+
+for ele in dic:
+    sum_dict += dic[ele]
+print(sum_dict if sum_dict <= k else 'Oh no')
