c8a7353a774fe45d34bccf2f6aa78c906ad85f39
Jin <149752689+kuru2141@users.noreply.github.com>
2025-03-02 02:25:07 +0900

    [Silver V] Title: 피카츄, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/14405. 피카츄/피카츄.py
+++ 백준/Silver/14405. 피카츄/피카츄.py
@@ -1,32 +1,10 @@
-from collections import deque
-
-def check_word(queue, word_list):
-    while queue:
-        a = queue.popleft()
-        if queue:
-            b = queue.popleft()
-            tmp = a + b
-            if tmp in word_list:
-                if tmp == 'ch':
-                    if queue:
-                        c = queue.popleft()
-                        if c != 'u':
-                            return 'NO'
-                    else:
-                        return 'NO'
-            else:
-                return 'NO'
-        else:
-            return 'NO'
-    return 'YES'
-
 S = input()
 
-S_set = set(S)
-queue = deque(S)
+S = S.replace('pi', ' ')
+S = S.replace('ka', ' ')
+S = S.replace('chu', ' ')
 
-if S_set <= set(['p', 'i', 'k', 'a', 'c', 'h', 'u']):
-    word_list = ['pi', 'ka', 'ch']
-    print(check_word(queue, word_list))
+if S == ' ' * len(S):
+    print('YES')
 else:
     print('NO')
