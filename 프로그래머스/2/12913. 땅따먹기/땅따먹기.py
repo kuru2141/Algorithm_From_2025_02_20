62501cb19bcf5ebdd7a8822dec4adac69ac67e11
Jin <149752689+kuru2141@users.noreply.github.com>
2025-03-02 14:07:24 +0900

    Update 땅따먹기.py

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -1,5 +1,4 @@
 def solution(land):
-
     for i in range(1, len(land)):
         for j in range(4):
             tmp = land[i][j]
@@ -7,8 +6,4 @@ def solution(land):
                 if j != k:
                     land[i][j] = max(land[i][j], tmp + land[i - 1][k])
     
-
     return max(land[-1])
-
-
-print(solution([[1,2,3,5],[5,6,7,8],[4,3,2,1]]))
