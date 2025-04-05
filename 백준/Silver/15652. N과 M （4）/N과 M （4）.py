3f8aa6202edcb8d22173f9e03c6ac8a3384398d7
Jin <149752689+kuru2141@users.noreply.github.com>
2025-04-06 03:14:24 +0900

    [Silver III] Title: N과 M (4), Time: 64 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,22 @@
+def combi(depth, M):
+    if len(numbers) == M:
+        for ele in numbers:
+            print(ele, end = ' ')
+        print()
+        return
+    
+    if depth > N - 1:
+        return
+
+    numbers.append(arr[depth])
+    combi(depth, M)
+    
+    numbers.remove(arr[depth])
+    combi(depth + 1, M)
+
+
+N, M = map(int, input().split())
+arr = [(i + 1) for i in range(N)]
+
+numbers = []
+combi(0, M)
