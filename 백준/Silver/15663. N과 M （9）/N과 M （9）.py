c0911355eb4affa30ecde1f83e5adc2f226caa40
Jin <149752689+kuru2141@users.noreply.github.com>
2025-04-06 14:48:48 +0900

    Update N과 M （9）.py

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.py
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -1,11 +1,30 @@
-from itertools import permutations
 import sys
 input = sys.stdin.readline
 
-N, M = map(int, input().split())
-arr = sorted(list(map(int, input().split())))
+def combi(depth):    
+    if len(numbers) == M:
+        result.add(tuple(numbers))
+        return
 
-for ele in sorted(list(set(permutations(arr, M)))):
+    for i in range(N):
+        if not visited[i]:
+            visited[i] = 1
+            numbers.append(arr[i])
+            combi(depth + 1)
+            visited[i] = 0
+            numbers.pop()
+
+N, M = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+
+result = set()
+visited = [0] * N
+numbers = []
+
+combi(0)
+
+for ele in sorted(result):
     for e in ele:
         print(e, end = ' ')
     print()
