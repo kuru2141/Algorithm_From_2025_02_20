4c5511b986f2d702e1a045b8131df285303bd757
Jin <149752689+kuru2141@users.noreply.github.com>
2025-04-13 14:02:03 +0900

    [Gold IV] Title: 네트워크 연결, Time: 260 ms, Memory: 123148 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
@@ -1,49 +1,31 @@
-class Edge:
-    def __init__(self, from_e, to_e, weight):
-        self.from_e = from_e
-        self.to_e = to_e
-        self.weight = weight
+import sys
+input = sys.stdin.readline
 
-def make(N):
-    parents = []
-    for i in range(N + 1):
-        parents.append(i)
-    return parents
+def find(x):
+    if parent[x] == x:
+        return x
+    parent[x] = find(parent[x])
+    return parent[x]
 
-def find(V):
-    if parents[V] == V:
-        return V
-    parents[V] = find(parents[V])
-    return parents[V]
+def union(x, y):
+    xRoot = find(x)
+    yRoot = find(y)
 
-def union(a, b, weight):
-    global total_weight
-    
-    aRoot = find(a)
-    bRoot = find(b)
-
-    if aRoot == bRoot:
-        return False
-
-    parents[aRoot] = bRoot
-    total_weight += weight
-    return True
-
+    if xRoot != yRoot:
+        parent[yRoot] = xRoot
 
 N = int(input())
 M = int(input())
 
-edgeList = []
-parents = make(N)
-total_weight = 0
+parent = [e for e in range(N + 1)]
 
-for _ in range(M):
-    from_e, to_e, weight = map(int, input().split())
-    edgeList.append(Edge(from_e, to_e, weight))
+arr = [tuple(map(int, input().split())) for _ in range(M)]
+arr.sort(key = lambda x: x[2])
 
-edgeList.sort(key = lambda x: x.weight)
+result = 0
+for a, b, c in arr:
+    if find(a) != find(b):
+        union(a, b)
+        result += c
 
-for edge in edgeList:
-    union(edge.from_e, edge.to_e, edge.weight)
-    
-print(total_weight)
+print(result)
