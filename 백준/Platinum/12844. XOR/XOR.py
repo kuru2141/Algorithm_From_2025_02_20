add86c9dc6ecefdb25a81f189b57732c5cc45f7d
Jin <149752689+kuru2141@users.noreply.github.com>
2025-04-20 02:59:27 +0900

    [Platinum III] Title: XOR, Time: 4252 ms, Memory: 209732 KB -BaekjoonHub

---
+++ 백준/Platinum/12844. XOR/XOR.py
@@ -0,0 +1,79 @@
+import math
+import sys
+input = sys.stdin.readline
+
+def segment(left, right, i = 1):
+    if left == right:
+        tree[i] = nums[left]
+        return tree[i]
+
+    mid = (left + right) // 2
+    tree[i] = segment(left, mid, i * 2) ^ segment(mid + 1, right, i * 2 + 1)
+    return tree[i]
+
+def propagation(start, end, i = 1):
+    if (end - start + 1) % 2:
+        tree[i] ^= lazy[i]
+
+    if start != end:
+        lazy[i * 2] ^= lazy[i]
+        lazy[i * 2 + 1] ^= lazy[i]
+
+    lazy[i] = 0
+
+def search(start, end, left, right, i = 1):
+    propagation(start, end, i)
+    
+    if left > end or right < start:
+        return 0
+
+    if left <= start and right >= end:
+        return tree[i]
+
+    mid = (start + end) // 2
+    return search(start, mid, left, right, i * 2) ^ search(mid + 1, end, left, right, i * 2 + 1)
+
+def update(start, end, left, right, val, i = 1):
+    propagation(start, end, i)
+
+    if left > end or right < start:
+        return
+
+    if left <= start and right >= end:
+        if (end - start + 1) % 2:
+            tree[i] ^= val
+        
+        if start != end:
+            lazy[i * 2] ^= val
+            lazy[i * 2 + 1] ^= val
+        return
+
+    mid = (start + end) // 2
+    update(start, mid, left, right, val, i * 2)
+    update(mid + 1, end, left, right, val, i * 2 + 1)
+
+    tree[i] = tree[i * 2] ^ tree[i * 2 + 1]
+
+N = int(input())
+nums = list(map(int, input().split()))
+
+tree_len = 2 ** (math.ceil(math.log(N, 2)) + 1)
+tree = [0] * tree_len
+lazy = [0] * tree_len
+
+segment(0, N - 1)
+
+M = int(input())
+for _ in range(M):
+    tmp = list(map(int, input().split()))
+
+    if tmp[0] == 1:
+        b, c, d = tmp[1:]
+        if b > c:
+            b, c = c, b
+        update(0, N - 1, b, c, d)
+    else:
+        b, c = tmp[1:]
+        if b > c:
+            b, c = c, b
+        print(search(0, N - 1, b, c))
