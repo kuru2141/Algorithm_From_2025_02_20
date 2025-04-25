f2df5bb67144b1ae4c2bfe068c3c459102b8eec5
Jin <149752689+kuru2141@users.noreply.github.com>
2025-04-25 17:01:12 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -0,0 +1,54 @@
+import sys
+input = sys.stdin.readline
+
+def calculator(num1, num2, cal):
+    if cal == '+':
+        return num1 + num2
+
+    if cal == '-':
+        return num1 - num2
+    
+sentence = input().strip()
+num = []
+cal = []
+
+i = 0
+tmp = ''
+while i < len(sentence):
+    x = sentence[i]
+    if x.isalnum():
+        tmp += x
+    else:
+        num.append(int(tmp))
+        cal.append(x)
+        tmp = ''
+    i += 1
+num.append(int(tmp))
+
+result = num[0]
+num = num[1:]
+tmp = 0
+
+check = 0
+for i in range(len(num)):
+    if cal[i] == '+':
+        if check == 0:
+            result += num[i]
+        else:
+            tmp += num[i]
+            if i == len(num) - 1:
+                result -= tmp
+            
+    else:
+        tmp += num[i]
+
+        if i == len(num) - 1:
+            result -= tmp
+        elif cal[i + 1] == '+':
+            check = 1
+        else:
+            result -= tmp
+            tmp = 0
+            check = 0
+
+print(result)
